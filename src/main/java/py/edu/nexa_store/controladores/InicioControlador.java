7919167e8510e54ef5e7882abda93c6bdd89d831
davidruizdiaz <adavidruizdiaz@gmail.com>
2026-09-17 11:48:28 -0300

    fix: cambio de menú tienda a detalle tienda

--- src/main/java/py/edu/nexa_store/controladores/InicioControlador.java
+++ src/main/java/py/edu/nexa_store/controladores/InicioControlador.java
@@ -1,17 +1,13 @@
 package py.edu.nexa_store.controladores;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import lombok.RequiredArgsConstructor;
 import py.edu.nexa_store.servicios.TiendaServicio;
 import py.edu.nexa_store.servicios.VendedorServicio;
 import py.edu.nexa_store.servicios.VentaServicio;
 
-@Controller
-@RequiredArgsConstructor
 public class InicioControlador {
 
     private final TiendaServicio tiendaServicio;
@@ -20,14 +16,15 @@ public class InicioControlador {
 
     @GetMapping("/")
     public String inicio() {
-        return "index";
+        return "";    // completar
     }
 
-    @GetMapping("/tienda/{id}/menu")
-    public String menu(@PathVariable Long id, Model model) {
+    // cambiar a TiendaControlador
+    @GetMapping("/tienda/ver/{id}")
+    public String ver(@PathVariable Long id, Model model) {
         model.addAttribute("tienda", tiendaServicio.buscarPorId(id));
         model.addAttribute("vendedores", vendedorServicio.listarPorTienda(id));
         model.addAttribute("ventas", ventaServicio.listarPorTienda(id));
-        return "tienda/menu";
+        return "tienda/detalle";
     }
 }
